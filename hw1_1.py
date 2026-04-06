9b9842c8047e547acdd101d952fc8ab1a71429ab
isakd <isak.djumaliev65@gmail.com>
2026-04-06 00:28:30 -0700

    hw2

--- hw1_1.py
+++ hw1_1.py
@@ -14,4 +14,16 @@ print("Результат:")
 total = monday + tuesday + wednesday + thursday + friday
 print("Всего было потрачено за неделю на расходы:", total)
 
-print("Средняя сумма расходов на продукты в день:", total / 5)
+average = total / 5
+
+print("Средняя сумма расходов на продукты в день:", average)
+
+
+if average >= 1 and average <= 100:
+    print("Малый расход")
+elif average >= 101 and average <= 400:
+    print ("Средний расход")
+elif average >= 401:
+    print("Высокий расход")
+else:
+    print("Ничего не потрачено")
---
+++ hw1_2.py
@@ -0,0 +1,49 @@
+
+def redColor(text: str):
+    return print(f'\033[31m{text} \033[0m')
+
+def greenColor(text: str):
+    return print(f'\033[32m{text} \033[0m')
+
+def yellowColor(text: str):
+    return print(f'\033[33m{text} \033[0m')
+
+try:
+    day =  int(input("Введите день рождения: "))
+    month = int(input("Введите месяц рождения: "))
+
+    if day >= 21 and month == 3 or day <= 20 and month == 4 :
+        greenColor("Овен")
+    elif day >= 21 and month == 4 or day <= 21 and month == 5:
+        greenColor("Телец")
+    elif day >= 22 and month == 5 or day <= 21 and month == 6:
+        greenColor("Близнецы")
+    elif day >= 22 and month == 6 or day <= 22 and month == 7:
+        greenColor("Рак")
+    elif day >= 23 and month == 7 or day <= 21 and month == 8:
+        greenColor("Лев")
+    elif day >= 22 and month == 8 or day <= 23 and month == 9:
+        greenColor("Дева")
+    elif day >= 24 and month == 9 or day <= 23 and month == 10:
+        greenColor("Весы")
+    elif day >= 24 and month == 10 or day <= 22 and month == 11:
+        greenColor("Скорпион")
+    elif day >= 23 and month == 11 or day <= 22 and month == 12:
+        greenColor("Стрелец")
+    elif day >= 23 and month == 12 or day <= 20 and month == 1:
+        greenColor("Козерог")
+    elif day >= 21 and month == 1 or day <= 19 and month == 2:
+        greenColor("Водолей")
+    elif day >= 20 and month == 2 or day <= 20 and month == 3:
+        greenColor("Рыбы")
+    else:
+        redColor("Ошибка, проверьте корректность введенной даты рождения")
+
+except:
+    redColor("Введите корректные данные как на примере ниже: ")
+    yellowColor("Введите день рождения: 10")
+    yellowColor("Введите месяц рождения: 12")
+  
+
+
+
